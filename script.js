22f919139521c897585d27607245f05eae137bdf
Michaelle <michaelle.strassegger@yahoo.com>
2026-05-30 11:14:48 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const suppen = [
+  "Gemüseboillon",
+  "Nudelsuppe",
+  "Gebundene Gemüsesuppe",
+  "Gemüseeintopf",
+  "Rest Day"
+];
+
+function pickItem() {
+  let randomIndex = Math.floor(Math.random() * suppen.length);
+  document.getElementById("result").innerText = suppen[randomIndex];
+}
+
+// Loop Beispiel (wichtig für dein Lernen!)
+for (let i = 0; i < suppen.length; i++) {
+  console.log("Option:", suppen[i]);
+}
